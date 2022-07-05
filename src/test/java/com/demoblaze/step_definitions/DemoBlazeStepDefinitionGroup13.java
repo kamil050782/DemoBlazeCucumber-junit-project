20754a1973a9b14e6451e756ec36ac3635627dff
tamir <tamir_alraee@yahoo.com>
2022-07-05 11:32:35 -0400

    commit from tamir

--- src/test/java/com/demoblaze/step_definitions/DemoBlazeStepDefinitionGroup13.java
+++ src/test/java/com/demoblaze/step_definitions/DemoBlazeStepDefinitionGroup13.java
@@ -1,4 +1,16 @@
 package com.demoblaze.step_definitions;
 
+import com.demoblaze.pages.DemoBlazePage;
+import com.demoblaze.utilities.ConfigurationReader;
+import com.demoblaze.utilities.Driver;
+import io.cucumber.java.en.Given;
+
 public class DemoBlazeStepDefinitionGroup13 {
+    DemoBlazePage demoBlazePage=new DemoBlazePage();
+    @Given("Customer navigation through product categories: Phones, Laptops and Monitors")
+    public void customer_navigation_through_product_categories_phones_laptops_and_monitors() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("demoBlaze.url"));
+    }
+
+
 }
